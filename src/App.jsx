5a67e4d36a3353a05a9c21716423e2b6f0162555
tteuwm <matheus.barreto@eaportal.org>
2024-03-27 21:16:58 -0300

    aplicacao das fontes

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,23 @@ export default function App(){
       <section>
         <div className="bg-orange">
           <img src ={iconSedans} alt="icone dos carros sedans" />
-          <h2>Sedans</h2>
-          <p>texto sedans, nao to conseguindo copiar</p>
-          <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+          <h2 className="font-shoulders">Sedans</h2>
+          <p className="font-lexend">texto sedans, nao to conseguindo copiar</p>
+          <a className="font-lexend" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
          
          <div className="bg-green">
           <img src={iconsSuvs} alt="Icone dos suvs" />
-          <h2>SUVs</h2>
-          <p>Texto suvs</p>
-          <a href="https://www.webmotors.com.br/suvs"target="_blank">Ver mais</a>
+          <h2 className="font-shoulders">SUVs</h2>
+          <p className="font-lexend">Texto suvs</p>
+          <a className="font-lexend" href="https://www.webmotors.com.br/suvs"target="_blank">Ver mais</a>
          </div>
 
         <div className="bg-dark-green">
           <img src={iconLuxury} alt="icone dos carros de luxo" />
-          <h2>Luxuosos</h2>
-          <p>texto do Luxuosos</p>
-          <a href="https://www.webmotors.com.br/carros-de-luxo"target="_blank">ver mais</a>
+          <h2 className="font-shoulders">Luxuosos</h2>
+          <p className="font-lexend">texto do Luxuosos</p>
+          <a className="font-lexend" href="https://www.webmotors.com.br/carros-de-luxo"target="_blank">ver mais</a>
         </div>
       </section>
     </main>
