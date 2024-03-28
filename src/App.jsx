eb88abec4f229a24c80b5042a92c37b3e6580193
tteuwm <matheus.barreto@eaportal.org>
2024-03-27 21:12:35 -0300

    Fontes

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ export default function App(){
   return(
     <main>
       <section>
-        <div>
+        <div className="bg-orange">
           <img src ={iconSedans} alt="icone dos carros sedans" />
           <h2>Sedans</h2>
           <p>texto sedans, nao to conseguindo copiar</p>
           <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
          
-         <div>
+         <div className="bg-green">
           <img src={iconsSuvs} alt="Icone dos suvs" />
           <h2>SUVs</h2>
           <p>Texto suvs</p>
           <a href="https://www.webmotors.com.br/suvs"target="_blank">Ver mais</a>
          </div>
 
-        <div>
+        <div className="bg-dark-green">
           <img src={iconLuxury} alt="icone dos carros de luxo" />
           <h2>Luxuosos</h2>
           <p>texto do Luxuosos</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ export default {
   theme: {
     colors: {
       'orange': '#e28625',
+      'green' : '#006971',
+      'dark-green' : '#004140',
+      'offwhite' : 'rgba(255, 255, 255, 0.75)',
+      'white' : '#fff',
+      'offwhitebg' : '#f2f2f2'
+
+    },
+    fontFamily: {
+      'shoulders' : '"Big Shoulders Display"',
+      'lexend': '"Lexend Deca"'
     }
   },
   plugins: [],
